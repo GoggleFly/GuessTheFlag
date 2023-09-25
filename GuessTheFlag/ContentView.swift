468a7a9a89ca5d9b96657d2881555ea0e52de2f7
David Ash <david.ash@ottid.co.uk>
2023-09-25 17:20:56 +0100

    Accessibility fix

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -60,6 +60,20 @@ struct ContentView: View {
         "UK",
         "US"].shuffled()
     
+    private let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     private let rounds = 3
     
     var body: some View {
@@ -75,6 +89,8 @@ struct ContentView: View {
                 Text("Guess the Flag")
                     .font(.largeTitle.bold())
                     .foregroundColor(.white)
+                
+                Spacer()
                 
                 VStack(spacing: 15) {
                     VStack {
@@ -101,6 +117,7 @@ struct ContentView: View {
                             
                         } label: {
                             FlagImage(country: countries[number])
+                                .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         }
                         .rotation3DEffect(.degrees(selected != nil && number == selected ? animationAmount : 0), axis: (x: 0, y: 1, z: 0))
                         .opacity(selected != nil && number != selected ? opacityAmount : 1)
@@ -120,11 +137,10 @@ struct ContentView: View {
                     }
                 }
                 .frame(maxWidth: .infinity)
-                .padding(.vertical, 20)
+                .padding(.vertical, 30)
                 .background(.regularMaterial)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .clipShape(RoundedRectangle(cornerRadius: 40))
                 
-                Spacer()
                 Spacer()
                 
                 Text("Score: \(score)")
